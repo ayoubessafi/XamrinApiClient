a7fc85cc56d7abbc4f501386fefff70068ef2919
ayoubessafi <57940108+ayoubessafi@users.noreply.github.com>
2021-06-16 15:08:16 +0100

    Add project files.

---
+++ XamWebApiClient/XamWebApiClient/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using XamWebApiClient.Services;
+using XamWebApiClient.ViewModels;
+
+namespace XamWebApiClient
+{
+    public static class Startup
+    {
+        private static IServiceProvider serviceProvider;
+        public static void ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            //add services
+            services.AddHttpClient<IBookService, ApiBookService>(c => 
+            {
+                c.BaseAddress = new Uri("http://10.0.2.2:52236/api/");
+                c.DefaultRequestHeaders.Add("Accept", "application/json");
+            });
+
+            //add viewmodels
+            services.AddTransient<BooksViewModel>();
+            services.AddTransient<AddBookViewModel>();
+            services.AddTransient<BookDetailsViewModel>();
+
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        public static T Resolve<T>() => serviceProvider.GetService<T>();
+    }
+}
---
+++ XamWebApiClient/XamWebApiClient/ViewModels/AddBookViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XamWebApiClient.Models;
+using XamWebApiClient.Services;
+
+namespace XamWebApiClient.ViewModels
+{
+    public class AddBookViewModel : BaseViewModel
+    {
+        private readonly IBookService _bookService;
+        private string title;
+        private string author;
+        private string description;
+
+        public AddBookViewModel(IBookService bookService)
+        {
+            _bookService = bookService;
+
+            SaveBookCommand = new Command(async () => await SaveBook());
+        }
+
+        private async Task SaveBook()
+        {
+            try
+            {
+                var book = new Book
+                {
+                    Title = Title,
+                    Author = Author,
+                    Description = Description
+                };
+
+                await _bookService.AddBook(book);              
+
+                await Shell.Current.GoToAsync("..");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public string Title
+        {
+            get => title; 
+            set
+            {
+                title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+        public string Author
+        {
+            get => author; 
+            set
+            {
+                author = value;
+                OnPropertyChanged(nameof(Author));
+            }
+        }
+        public string Description
+        {
+            get => description; 
+            set
+            {
+                description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+
+        public ICommand SaveBookCommand { get; }
+    }
+}
